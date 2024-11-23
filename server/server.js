e29c5222953dc28ce8d3f50839287d5ebe74d8c3
seyigeorge <seyigeorge88@gmail.com>
2024-11-24 00:16:03 +0100

    Your commit message

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+require("dotenv").config();
 const app = express();
 const PORT = 5000;
 
